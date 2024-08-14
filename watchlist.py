8d35af10dee61cf2d28994a079b0a2ad4cd7834d
Marco T. <107734315+marknovanta@users.noreply.github.com>
2024-08-14 09:34:49 +0200

    Update watchlist

--- watchlist.py
+++ watchlist.py
@@ -8,7 +8,7 @@ tickers_list = [
     'GPC','EMR','CL','NOC','NFLX','DIS','TSLA','HD','COST','XOM','SHEL',
     'MA','V','LLY','GE','CAT','UNP','UPS','PLD','AMT','EQIX','NEE','SO',
     'DUK','HRL','ITW','ABT','SWK','BDX','KMB','PPG','TGT','GWW','ADM','NUE',
-    'PEP','SPGI','PFE','VOW3.DE','MBG.DE','CAG','NYCB','ABR','NWL','DVN',
+    'PEP','SPGI','PFE','CAG','NYCB','ABR','NWL','DVN',
     'MO','PARA','MCO','AVGO','SEIC','VICI','ARE','EPRT','CCI','ADC','ANSS',
     'CB','DHI','TEL','PHM','SNA','STT','AWR','NWN','UGI','OXY'
 ]
