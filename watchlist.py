bdae894bcb07dfe7bc3c99415d4064c3d5fb75f6
Marco T. <107734315+marknovanta@users.noreply.github.com>
2024-08-14 09:32:20 +0200

    Update watchlist

--- watchlist.py
+++ watchlist.py
@@ -1,4 +1,14 @@
 tickers_list = [
-    'SWK','SYY','UVV','GRC','MDT','FELE','UMBF','AOS',
-    'PBF','FAST','GGG','AAON'
+    'UVV','GRC','MDT','FELE','UMBF','AOS',
+    'PBF','FAST','GGG','AAON',
+    'WPC','NNN','EPR','FRT','O','GOOD','BLK','KO','JNJ','PG','AAPL',
+    'MCD','LMT','AXP','SBUX','INTC','VZ','AFL','SRCE','MAIN','CMCSA','CVX',
+    'JPM','AMZN','GOOGL','MSFT','FDX','NVDA','WMT','ADP','UNH','HPQ','C',
+    'GS','MMM','BAC','KR','PRU','SYY','ABBV','INTU','LOW','CINF','DOV',
+    'GPC','EMR','CL','NOC','NFLX','DIS','TSLA','HD','COST','XOM','SHEL',
+    'MA','V','LLY','GE','CAT','UNP','UPS','PLD','AMT','EQIX','NEE','SO',
+    'DUK','HRL','ITW','ABT','SWK','BDX','KMB','PPG','TGT','GWW','ADM','NUE',
+    'PEP','SPGI','PFE','VOW3.DE','MBG.DE','CAG','NYCB','ABR','NWL','DVN',
+    'MO','PARA','MCO','AVGO','SEIC','VICI','ARE','EPRT','CCI','ADC','ANSS',
+    'CB','DHI','TEL','PHM','SNA','STT','AWR','NWN','UGI','OXY'
 ]
